963e16020d74818cfb4e2478075dfe8d7929544a
BabuRoyC <prcbubu@gmail.com>
2023-04-14 19:21:08 +0530

    Added background to the heading as well as changed the heading font and colour

--- app/src/main/java/com/example/unitconterto/MainActivity.java
+++ app/src/main/java/com/example/unitconterto/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.unitconterto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Animation obj = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+        TextView heading =  (TextView) findViewById(R.id.appName);
+        heading.startAnimation(obj);
     }
 }
